57c4531d0b5707c0bf8c0ec4b933ce32835fb87f
Jonus <joclapshaw@gmail.com>
2025-10-28 10:18:16 -0700

    o

--- app.js
+++ app.js
@@ -1,23 +1,76 @@
+// Import the express module
 import express from 'express';
 
-//create an instance of an express application
+// Create an instance of an Express application
 const app = express();
 
 // Enable static file serving
 app.use(express.static('public'));
 
-//Define the port number where our server will listen 
+// Allow the app to parse form data (req.body)
+app.use(express.urlencoded({ extended: true }));
+
+// Create an array to store orders
+const orders = [];
+
+// Define the port number where our server will listen
 const PORT = 3000;
 
-//Define a default "route" ('/')
-//req: contains information about the incoming request
-//res: allows us to send back a response to the client
-app.get('/', (req, res) => {
-    //res.send('Welcome to Poppa\'s Pizza!');
-    res.sendFile(`${import.meta.dirname}/views/home.html`);
-})
+app.set('view engine', 'ejs');
 
-//Start the server and listen on the specified port
+// Define a default "route" ('/')
+// req: contains information about the incoming request
+// res: allows us to send back a response to the client
+app.get('/', (req, res) => {
+
+    // Send a response to the client
+    // res.send(`<h1>Welcome to Poppa\'s Pizza!</h1>`);
+    res.render('home');
+});
+
+// Define a "contact-us" route
+app.get('/contact-us', (req, res) => {
+    res.sendFile(`${import.meta.dirname}/views/contact.html`);
+});
+
+// Define a "confirmation" route
+app.get('/confirm', (req, res) => {
+    res.sendFile(`${import.meta.dirname}/views/confirmation.html`);
+});
+
+// Define an "admin" route
+app.get('/admin', (req, res) => {
+
+    res.send(orders);
+    //res.sendFile(`${import.meta.dirname}/views/admin.html`);
+});
+
+// Define an "submit-order" route
+app.post('/submit-order', (req, res) => {
+
+    //console.log(req.body);
+
+    // Create a JSON object to store the data
+    const order = {
+        fname: req.body.fname,
+        lname: req.body.lname,
+        email: req.body.email,
+        method: req.body.method,
+        toppings: req.body.toppings,
+        size: req.body.size,
+        comment: req.body.comment
+      };
+
+      // Add order to array
+      orders.push(order);
+      console.log(orders);
+
+      // Send user to confirmation page
+      res.sendFile(`${import.meta.dirname}/views/confirmation.html`);
+});
+
+// Start the server and make it listen on the port 
+// specified above
 app.listen(PORT, () => {
-    console.log(`Server is running at http:localhost:${PORT}`);
-})
+    console.log(`Server is running at http://localhost:${PORT}`);
+}); 
